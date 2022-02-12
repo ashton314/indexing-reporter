7bb9d1bb436b18a359ad4855b084b92c076b979d
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2022-02-12 14:09:48 -0700

    Admin scoreboard working

--- lib/indexing_reporter_web/live/admin_live/admin_index.ex
+++ lib/indexing_reporter_web/live/admin_live/admin_index.ex
@@ -2,8 +2,9 @@ defmodule IndexingReporterWeb.AdminIndexLive.AdminIndex do
   use IndexingReporterWeb, :live_view
   on_mount IndexingReporterWeb.UserLiveAuth
 
-  alias IndexingReporter.Logbook
   alias IndexingReporter.Logbook.IndexLog
+  alias IndexingReporter.Accounts.User
+  alias IndexingReporter.Repo
 
   @impl true
   def mount(_params, _session, socket) do
@@ -15,25 +16,28 @@ defmodule IndexingReporterWeb.AdminIndexLive.AdminIndex do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
-  # defp apply_action(socket, :edit, %{"id" => id}) do
-  #   socket
-  #   |> assign(:page_title, "Edit Index log")
-  #   |> assign(:index_log, Logbook.get_index_log!(id))
-  # end
-
-  # defp apply_action(socket, :new, _params) do
-  #   socket
-  #   |> assign(:page_title, "New Index log")
-  #   |> assign(:index_log, %IndexLog{})
-  #   |> assign(:current_user, socket.assigns.current_user)
-  # end
-
   defp apply_action(socket, :index, _params) do
-    logs = Logbook.list_index_logs(socket.assigns.current_user.id)
-    total = logs |> Enum.reduce(0, fn %IndexLog{count: c}, acc -> c + acc end)
+    {total, listing} = get_stats()
 
     socket
     |> assign(:page_title, "Admin | Top Indexers")
     |> assign(:total_indexing, total)
+    |> assign(:listing, listing)
+  end
+
+  def get_stats() do
+    import Ecto.Query
+
+    listing_query = from u in User,
+      join: l in IndexLog, on: u.id == l.user_id,
+      group_by: u.id,
+      order_by: [desc: sum(l.count), asc: u.id],
+      select: %{id: u.id, name: u.name, email: u.email, total: sum(l.count)}
+      # select: {u.name, over(sum(l.count), partition_by: u.id)}
+
+    total_query = from l in IndexLog, select: {sum(l.count)}
+    {total} = Repo.one(total_query)
+
+    {total, Repo.all(listing_query)}
   end
 end
